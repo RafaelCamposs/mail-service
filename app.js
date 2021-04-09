243ed0c225f12913ea1abd61f9036bd3710f71bb
Rafael Campos <rafacampos2000@hotmail.com>
2021-04-09 18:14:17 -0300

    sending HTML with nodemailer

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const sendMailController = require('./mailService')
 
 const mailMan = new sendMailController();
 app.get('/', mailMan.send);
-
+app.get('/html',mailMan.sendHTML)
 
 module.exports = app;
--- mailService.js
+++ mailService.js
@@ -29,6 +29,34 @@ class SendMail {
         });
     }
 
+    async sendHTML(req,res) {
+
+        var transport = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "",
+                pass: ""
+            }
+        });
+
+        const message = {
+            from: 'elonmusk@tesla.com', 
+            to: 'to@email.com',         
+            subject: 'Design Your Model S | Tesla', 
+            html: '<h1>Have the most fun you can in a car!</h1><p>Get your <b>Tesla</b> today!</p>'
+        };
+
+        transport.sendMail(message, function(err, info) {
+            if (err) {
+              res.json('err');
+            } else {
+              res.json(info);
+            }
+        });
+    }
+    
+
 }
 
 module.exports = SendMail
