55ca69e7651123ee0ea80da75058e23a4be65914
Rafael Campos <rafacampos2000@hotmail.com>
2021-04-08 22:15:33 -0300

    Sending mail with nodemailer using mailtrap as SMTP

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const app = express();
 
+const nodemailer = require('nodemailer');
+const sendMailController = require('./mailService')
+
+const mailMan = new sendMailController();
+app.get('/', mailMan.send);
+
+
 module.exports = app;
---
+++ mailService.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer')
+
+class SendMail {
+
+    async send(req,res) {
+
+        var transport = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "",
+                pass: ""
+            }
+        });
+
+        const message = {
+            from: 'elonmusk@tesla.com', 
+            to: 'to@email.com',         
+            subject: 'Design Your Model S | Tesla', 
+            text: 'Have the most fun you can in a car. Get your Tesla today!'
+        };
+
+        transport.sendMail(message, function(err, info) {
+            if (err) {
+              res.json('err');
+            } else {
+              res.json(info);
+            }
+        });
+    }
+
+}
+
+module.exports = SendMail
